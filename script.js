c5b7725be68771c77840c26a76685a4ad5b4a2cd
Deanna Mandarino <mandarinodeanna@gmail.com>
2025-01-07 15:13:12 -0500

    Added functionality for a mute button

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const progressBar = player.querySelector('.video-player__controls--progress-bar_
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('.skip');
 const ranges = player.querySelectorAll('.video-player__controls--slider');
+const mute = player.querySelector('.mute');
 
 /* Build Our Functions */
 // Function to toggle play/pause
@@ -29,6 +30,15 @@ function skip() {
 // Handle range input changes (volume/speed)
 function handleRangeUpdate() {
   video[this.name] = this.value;
+
+  if (this.name === 'volume' && this.value == 0) {
+    video.muted = true;
+    mute.innerHTML = `<i class="fa-solid fa-volume-xmark"></i>`;
+  } 
+  else {
+    video.muted = false;
+    mute.innerHTML = `<i class="fa-solid fa-volume-high"></i>`;
+  }
 }
 
 // Update progress bar
@@ -41,6 +51,33 @@ function handleProgress() {
 function scrub(event) {
   const scrubTime = (event.offsetX / progress.offsetWidth) * video.duration;
   video.currentTime = scrubTime;
+}
+
+// Function to toggle mute/unmute
+function toggleMute() {
+  video.muted = !video.muted;
+
+  const icon = video.muted
+    ? `<i class="fa-solid fa-volume-xmark"></i>`
+    : `<i class="fa-solid fa-volume-high"></i>`;
+  mute.innerHTML = icon;
+
+  if (video.muted) {
+    // If muted, set volume slider to 0
+    ranges.forEach(range => {
+      if (range.name === 'volume') {
+        range.value = 0;
+      }
+    });
+  } 
+  else {
+    // If unmuted, restore volume from previous state
+    ranges.forEach(range => {
+      if (range.name === 'volume') {
+        range.value = video.volume;
+      }
+    });
+  }
 }
 
 /* Hook Up the Event Listeners */
@@ -68,3 +105,6 @@ progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (event) => mousedown && scrub(event));
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
+
+// Event listener for mute/unmute
+mute.addEventListener('click', toggleMute);
